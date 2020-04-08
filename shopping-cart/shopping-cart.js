b94dcc1f4492e08dbca1b8650e0c6f5adc6cac35
keegers <nikkikiga@gmail.com>
2020-04-08 14:16:50 -0700

    add place order behavior

--- shopping-cart/shopping-cart.js
+++ shopping-cart/shopping-cart.js
@@ -5,19 +5,39 @@ import { findById, calcOrderTotal } from '../common/utils.js';
 
 const cartTable = document.getElementById('cart-body');
 const orderTotalCell = document.getElementById('order-total-dest');
+const placeOrder = document.getElementById('submit-order');
 
-const cart = JSON.parse(localStorage.getItem('CART'));
+//Nab the cart from localStorage as a string
+const stringCart = localStorage.getItem('CART');
 
-cart.forEach(item => {
-    //find the matching product
-    const product = findById(products, item.id);
+//If there are things in the cart then render the cart
+if (stringCart) {
+    const cart = JSON.parse(stringCart);
+    cart.forEach(item => {
+        //find the matching product
+        const product = findById(products, item.id);
+        
+        //call and receive returned dom
+        const cartRow = renderLineItem(item, product);
     
-    //call and receive returned dom
-    const cartRow = renderLineItem(item, product);
+        //attach to cart container
+        cartTable.appendChild(cartRow);
+    });
+    const orderTotal = calcOrderTotal(cart, products);
+    orderTotalCell.textContent = orderTotal;
+    placeOrder.addEventListener('click', () => {
+        //display alert with contents of cart JSON.stringify(cart, true, 2)
+        alert(`Cart Contents: ${JSON.stringify(cart, true, 2)}`);
+        
+        //Remove the cart from local storage with .removeItem
+        localStorage.removeItem('CART');
 
-    //attach to cart container
-    cartTable.appendChild(cartRow);
-});
+        //Redirect user back to home page
+        window.location.href = '../';
+    });
+} else {
+    
+    //Disable the place order button since cart is empty
+    placeOrder.disabled = true;
+}
 
-const orderTotal = calcOrderTotal(cart, products);
-orderTotalCell.textContent = orderTotal;
